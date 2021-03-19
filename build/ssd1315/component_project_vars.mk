2fbd67d55a9b186d8a8a0b81a236c2f13551d8a2
Adrian Bradianu <abradianu@gmail.com>
2021-03-19 21:39:17 +0200

    ups: remove build folder

--- build/ssd1315/component_project_vars.mk
+++
@@ -1,7 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/ssd1315
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/ssd1315 -lssd1315
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += ssd1315
-component-ssd1315-build: 
